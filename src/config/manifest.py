6f278fa0e28e193b1ad5406e1fc5234db38a36c8
bachnxuan <bachnxuan@gmail.com>
2025-06-30 19:36:36 +0700

    feat({source, manifest}): Introduce git link simplifier
    Signed-off-by: bachnxuan <bachnxuan@gmail.com>

--- src/config/manifest.py
+++ src/config/manifest.py
@@ -1,32 +1,33 @@
 from .config import WORKSPACE, TOOLCHAIN
 from typing import Final
 
+
 KERNEL: Final[dict[str, str]] = {
-    "url": "https://github.com/bachnxuan/android12-5.10-lts",
+    "url": "github.com:bachnxuan/android12-5.10-lts",
     "branch": "android12-5.10-lts",
     "to": str(WORKSPACE),
 }
 
 ANYKERNEL: Final[dict[str, str]] = {
-    "url": "https://github.com/bachnxuan/AnyKernel3",
+    "url": "github.com:bachnxuan/AnyKernel3",
     "branch": "android12-5.10",
     "to": str(WORKSPACE / "AnyKernel3"),
 }
 
 BUILD_TOOL: Final[dict[str, str]] = {
-    "url": "https://android.googlesource.com/kernel/prebuilts/build-tools",
+    "url": "android.googlesource.com:kernel/prebuilts/build-tools",
     "branch": "main-kernel-build-2024",
     "to": str(TOOLCHAIN / "build-tools"),
 }
 
 MKBOOTIMG: Final[dict[str, str]] = {
-    "url": "https://android.googlesource.com/platform/system/tools/mkbootimg",
+    "url": "android.googlesource.com:platform/system/tools/mkbootimg",
     "branch": "main-kernel-build-2024",
     "to": str(TOOLCHAIN / "mkbootimg"),
 }
 
 CLANG: Final[dict[str, str]] = {
-    "url": "https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379.git",
+    "url": "gitlab.com:crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379",
     "branch": "15.0",
     "to": str(TOOLCHAIN / "clang"),
 }
--- src/utils/source.py
+++ src/utils/source.py
@@ -1,7 +1,10 @@
+import requests
+
 from dataclasses import dataclass, field
 from subprocess import CompletedProcess
 from src.config.manifest import SOURCES
 from src.utils.shell import Shell
+from urllib.parse import ParseResult, urlparse, urlunparse
 from src.utils.log import log
 from typing import TypeAlias
 
@@ -12,6 +15,29 @@ Proc: TypeAlias = CompletedProcess[bytes]
 class SourceManager:
     shell: Shell = field(default_factory=Shell)
     sources: list[dict[str, str]] = field(default_factory=lambda: SOURCES.copy())
+
+    def git_simplifier(
+        self, repo: dict[str, str] | None = None, url: str | None = None
+    ) -> str | None:
+        repo_url: str | None = None
+        if repo and repo.get("url", None):
+            repo_url = repo["url"]
+        elif url:
+            repo_url = url
+        else:
+            return repo_url
+
+        with requests.get(repo_url) as resp:
+            parsed: ParseResult = urlparse(resp.url)
+            return f"{parsed.netloc}:{parsed.path.strip('/')}"
+
+    def restore_simplified(self, simplified: str) -> str:
+        if simplified.startswith(("http://", "https://")):
+            url = simplified
+        else:
+            host, repo = simplified.split(":", 1)
+            url = urlunparse(("https", host, "/" + repo, "", "", ""))
+        return url
 
     def clone_repo(
         self, repo: dict[str, str], *, depth: int = 1, args: list[str] | None = None
@@ -33,7 +59,7 @@ class SourceManager:
                 "-b",
                 repo["branch"],
                 *(args or []),
-                repo["url"],
+                self.restore_simplified(repo["url"]),
                 repo["to"],
             ]
         )
