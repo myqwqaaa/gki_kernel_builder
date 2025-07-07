d12dcb1632dbb7aa74c2f06347cf6182c577657c
bachnxuan <bachnxuan@gmail.com>
2025-07-07 22:01:05 +0700

    fix(susfs): Use official KernelSU patch
    Signed-off-by: bachnxuan <bachnxuan@gmail.com>

--- kernel_builder/pre_build/susfs.py
+++ kernel_builder/pre_build/susfs.py
@@ -2,7 +2,7 @@ import shutil
 import os
 
 from subprocess import CompletedProcess
-from kernel_builder.config.config import PATCHES, WORKSPACE
+from kernel_builder.config.config import WORKSPACE
 from kernel_builder.utils.env import ksu_variant, susfs_enabled
 from kernel_builder.utils.shell import Shell
 from kernel_builder.utils.log import log
@@ -37,7 +37,7 @@ class SUSFSPatcher:
 
         SUSFS: Path = WORKSPACE / "susfs4ksu" / "kernel_patches"
         GKI_SUSFS: Path = SUSFS / "50_add_susfs_in_gki-android12-5.10.patch"
-        KSUN_SUSFS: Path = PATCHES / "ksun_susfs.patch"
+        KSU_SUSFS: Path = SUSFS / "KernelSU" / "10_enable_susfs_for_ksu.patch"
 
         log("Applying kernel-side SUSFS patches")
         self.copy(SUSFS / "fs", WORKSPACE / "fs")
@@ -48,7 +48,7 @@ class SUSFSPatcher:
         if self.ksu_variant == "NEXT":
             orig_cwd: Path = Path.cwd()
             os.chdir(WORKSPACE / "KernelSU-Next")
-            self.shell.patch(KSUN_SUSFS)
+            self.shell.patch(KSU_SUSFS)
             os.chdir(orig_cwd)
 
 
